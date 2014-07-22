75d00dec16c5ec585ae84561dcf502ab5e9336fc
raphael meyer <raphael.meyer@bbv.ch>
2014-07-22 07:56:06 +0200

    Initial commit

---
+++ app/src/main/java/ch/quazz/caverna/CountingInput.java
@@ -0,0 +1,9 @@
+package ch.quazz.caverna;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by raphael on 21/07/14.
+ */
+public class CountingInput extends LinearLayout {
+}
---
+++ app/src/main/java/ch/quazz/caverna/PlayerScoreActivity.java
@@ -0,0 +1,96 @@
+package ch.quazz.caverna;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import ch.quazz.caverna.R;
+
+public class PlayerScoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_score);
+
+        initTabs();
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekbar_dwarfs);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView dwarfs = (TextView)findViewById(R.id.number_of_dwarfs);
+                dwarfs.setText(Integer.toString(seekBar.getProgress() + 2));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar = (SeekBar)findViewById(R.id.seekbar_donkeys);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView donkeys = (TextView)findViewById(R.id.number_of_donkeys);
+                donkeys.setText(Integer.toString(seekBar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void initTabs() {
+        TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+        tabHost.setup();
+
+        tabHost.addTab(tabHost.newTabSpec("Resources").setIndicator("Resources")
+                .setContent(R.id.tab1));
+
+        tabHost.addTab(tabHost.newTabSpec("Landscape").setIndicator("Landscape")
+                .setContent(R.id.tab2));
+
+        tabHost.addTab(tabHost.newTabSpec("Furnishing").setIndicator("Furnishing")
+                .setContent(R.id.tab3));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.player_score, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
