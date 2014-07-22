2e54935d78f974f1d2d84c15f456703b97672c5e
raphael meyer <raphael.meyer@bbv.ch>
2014-07-22 23:44:21 +0200

    Experimenting with a composite ui component

--- app/src/main/java/ch/quazz/caverna/CountingInput.java
+++ app/src/main/java/ch/quazz/caverna/CountingInput.java
@@ -1,9 +1,18 @@
 package ch.quazz.caverna;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
-/**
- * Created by raphael on 21/07/14.
- */
 public class CountingInput extends LinearLayout {
+
+    public CountingInput(Context context, AttributeSet attr) {
+        super(context, attr);
+
+        final LayoutInflater inflater =
+                (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.counting_input, this);
+    }
+
 }
