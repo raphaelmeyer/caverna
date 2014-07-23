1e24e88aa1595af0188d06d8810a07530495d7af
Raphael Meyer <raphael.meyer@bbv.ch>
2014-07-23 08:26:34 +0200

    Add first test

--- app/src/test/java/ch/quazz/caverna/test/PlayerTest.java
+++ app/src/test/java/ch/quazz/caverna/test/PlayerTest.java
@@ -2,8 +2,19 @@ package ch.quazz.caverna.test;
 
 import android.test.InstrumentationTestCase;
 
+import ch.quazz.caverna.Player;
+
 public class PlayerTest extends InstrumentationTestCase {
+
+    private Player testee;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        testee = new Player();
+    }
+
     public void test() throws Exception {
-        assertFalse(true);
+        assertEquals(testee.score(), 0);
     }
 }
