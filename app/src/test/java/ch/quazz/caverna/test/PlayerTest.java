7d00df2354fba26912fbc25922e4cf110639c905
Raphael Meyer <raphael.meyer@bbv.ch>
2014-07-23 07:32:17 +0200

    Update project to include unit test package

---
+++ app/src/test/java/ch/quazz/caverna/test/PlayerTest.java
@@ -0,0 +1,9 @@
+package ch.quazz.caverna.test;
+
+import android.test.InstrumentationTestCase;
+
+public class PlayerTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        assertFalse(true);
+    }
+}
