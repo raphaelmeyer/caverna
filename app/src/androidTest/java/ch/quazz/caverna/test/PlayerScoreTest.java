192fa5501b9a309b5027062e9d04d0bd878e9f95
raphael meyer <raphael.meyer@bbv.ch>
2014-08-06 21:39:32 +0200

    Use hamcrest notiation

--- app/src/androidTest/java/ch/quazz/caverna/test/PlayerScoreTest.java
+++ app/src/androidTest/java/ch/quazz/caverna/test/PlayerScoreTest.java
@@ -26,30 +26,30 @@ public class PlayerScoreTest extends AndroidTestCase {
     public void test_the_initial_count_of_anything_else_than_dwarfs_is_zero() {
         for (PlayerScore.Item item : PlayerScore.Item.values()) {
             if (item != PlayerScore.Item.Dwarfs) {
-                assertEquals(0, testee.getCount(item));
+                assertThat(testee.getCount(item), equalTo(0));
             }
         }
     }
 
     public void test_initial_score_is_two_dwarfs_and_missing_farm_animal_cost() {
-        assertEquals(initialScore, testee.score());
+        assertThat(testee.score(), equalTo(initialScore));
     }
 
     public void test_each_dwarf_scores_a_point() {
         final int initialScoreWithoutDwarfs = initialScore - initialScoreDwarfs;
 
         testee.setCount(PlayerScore.Item.Dwarfs, 3);
-        assertEquals(initialScoreWithoutDwarfs + 3, testee.score());
+        assertThat(testee.score(), equalTo(initialScoreWithoutDwarfs + 3));
 
         testee.setCount(PlayerScore.Item.Dwarfs, 5);
-        assertEquals(initialScoreWithoutDwarfs + 5, testee.score());
+        assertThat(testee.score(), equalTo(initialScoreWithoutDwarfs + 5));
     }
 
     public void test_each_missing_type_of_farm_animal_costs_two_points() {
         final int initialScoreWithoutFarmAnimals = initialScore - initialScoreFarmAnimals;
 
         final int farmAnimals = 4;
-        assertEquals(initialScoreWithoutFarmAnimals - (farmAnimals * 2), testee.score());
+        assertThat(testee.score(), equalTo(initialScoreWithoutFarmAnimals - (farmAnimals * 2)));
     }
 
     public void test_each_animal_scores_a_point() {
@@ -61,46 +61,46 @@ public class PlayerScoreTest extends AndroidTestCase {
         testee.setCount(PlayerScore.Item.Boars, 1);
         testee.setCount(PlayerScore.Item.Cattle, 1);
 
-        assertEquals(initialScoreWithoutFarmAnimals + 5, testee.score());
+        assertThat(testee.score(), equalTo(initialScoreWithoutFarmAnimals + 5));
     }
 
     public void test_setting_a_farm_animal_count_back_to_zero_readds_the_cost() {
         testee.setCount(PlayerScore.Item.Sheep, 1);
         testee.setCount(PlayerScore.Item.Sheep, 0);
 
-        assertEquals(initialScore, testee.score());
+        assertThat(testee.score(), equalTo(initialScore));
     }
 
     public void test_each_small_pasture_scores_two_points() {
         testee.setCount(PlayerScore.Item.SmallPastures, 1);
-        assertEquals(initialScore + 2, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 2));
 
         testee.setCount(PlayerScore.Item.SmallPastures, 3);
-        assertEquals(initialScore + 6, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 6));
     }
 
     public void test_each_large_pasture_scores_four_points() {
         testee.setCount(PlayerScore.Item.LargePastures, 1);
-        assertEquals(initialScore + 4, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 4));
 
         testee.setCount(PlayerScore.Item.LargePastures, 3);
-        assertEquals(initialScore + 12, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 12));
     }
 
     public void test_each_ore_mine_scores_three_points() {
         testee.setCount(PlayerScore.Item.OreMines, 1);
-        assertEquals(initialScore + 3, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 3));
 
         testee.setCount(PlayerScore.Item.OreMines, 3);
-        assertEquals(initialScore + 9, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 9));
     }
 
     public void test_each_ruby_mine_scores_four_points() {
         testee.setCount(PlayerScore.Item.RubyMines, 1);
-        assertEquals(initialScore + 4, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 4));
 
         testee.setCount(PlayerScore.Item.RubyMines, 2);
-        assertEquals(initialScore + 8, testee.score());
+        assertThat(testee.score(), equalTo(initialScore + 8));
     }
 
     public void test_each_pair_of_grains_scores_a_point() {
