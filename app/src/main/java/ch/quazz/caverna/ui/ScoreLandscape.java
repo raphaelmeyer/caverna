a7fdb8d35420b49cb19f514377577fdd54703388
Raphael Meyer <raphael.meyer@bbv.ch>
2014-08-07 21:42:19 +0200

    Prototype of landscape tab

--- app/src/main/java/ch/quazz/caverna/ui/ScoreLandscape.java
+++ app/src/main/java/ch/quazz/caverna/ui/ScoreLandscape.java
@@ -12,8 +12,13 @@ import ch.quazz.caverna.R;
 import ch.quazz.caverna.widget.CountingInput;
 
 public class ScoreLandscape extends Fragment {
+    private final ItemCountController.ItemCount itemCounts[] = {
+            new ItemCountController.ItemCount(R.id.small_pastures, GameItem.SmallPastures),
+            new ItemCountController.ItemCount(R.id.large_pastures, GameItem.LargePastures)
+    };
 
     private PlayerScore playerScore;
+    private ItemCountController itemCount;
 
     public ScoreLandscape() {
     }
@@ -25,36 +30,12 @@ public class ScoreLandscape extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_score_landscape, container, false);
-
-
-        CountingInput smallPastures = (CountingInput)view.findViewById(R.id.small_pastures);
-        smallPastures.addOnCountChangeListener(new CountingInput.OnCountChangeListener() {
-            @Override
-            public void onCountChanged(int count) {
-                playerScore.setCount(GameItem.SmallPastures, count);
-            }
-        });
-
-        CountingInput largePastures = (CountingInput)view.findViewById(R.id.large_pastures);
-        largePastures.addOnCountChangeListener(new CountingInput.OnCountChangeListener() {
-            @Override
-            public void onCountChanged(int count) {
-                playerScore.setCount(GameItem.LargePastures, count);
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_score_landscape, container, false);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
-        CountingInput smallPastures = (CountingInput)getActivity().findViewById(R.id.small_pastures);
-        CountingInput largePastures = (CountingInput)getActivity().findViewById(R.id.large_pastures);
-
-        smallPastures.setCount(playerScore.getCount(GameItem.SmallPastures));
-        largePastures.setCount(playerScore.getCount(GameItem.LargePastures));
+        itemCount = new ItemCountController(playerScore, itemCounts, getActivity());
     }
 }
