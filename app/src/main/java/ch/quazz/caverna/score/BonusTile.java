4e7813f5bce2355936ecef4cfc5678646a85af4a
raphael meyer <raphael.meyer@bbv.ch>
2014-08-14 22:44:47 +0200

    Add bonus tiles

---
+++ app/src/main/java/ch/quazz/caverna/score/BonusTile.java
@@ -0,0 +1,24 @@
+package ch.quazz.caverna.score;
+
+public enum BonusTile {
+    WeavingParlor,
+    MilkingParlor,
+    StateParlor,
+    HungtingParlor,
+    BeerParlor,
+    BlacksmithingParlor,
+
+    StoneStorage,
+    OreStorage,
+    SparePartStorage,
+    MainStorage,
+    WeaponStorage,
+    SuppliesStorage,
+
+    BroomChamber,
+    TreasureChamber,
+    FoodChamber,
+    PrayerChamber,
+    WritingChamber,
+    FodderChamber
+}
